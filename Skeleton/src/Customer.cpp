e7e223b279065d1bf912430dd8556ce683d64c42
gilbarel1 <gilbare@post.bgu.ac.il>
2024-01-23 13:22:58 +0200

    added row

--- Skeleton/src/Customer.cpp
+++ Skeleton/src/Customer.cpp
@@ -10,6 +10,7 @@ int Customer::getMaxOrders() const {return maxOrders;}
 int Customer::getNumOrders() const {return ordersId.size();} //Returns num of orders the customer has made so far
 bool Customer::canMakeOrder() const {return (getNumOrders() < maxOrders);} //Returns true if the customer didn't reach max orders
 const vector<int>& Customer::getOrdersIds() const {return ordersId;}
+
 int Customer::addOrder(int orderId) { //return OrderId if order was added successfully, -1 otherwise
     if(canMakeOrder()) {ordersId.push_back(orderId);}
     else return -1;
